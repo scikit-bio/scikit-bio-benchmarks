15315b451a71e67b888715300359b3abcfea8026
Matt Aton <matthewaton2@gmail.com>
2025-11-19 13:35:43 -0700

    Add tree benchmarks

---
+++ benchmarks/tree.py
@@ -0,0 +1,41 @@
+"""Benchmarks on skbio.tree"""
+
+from skbio.stats.distance import randdm
+from skbio.tree import nj
+from asv_runner.benchmarks.mark import skip_benchmark_if
+
+
+class Tree:
+    def setup(self):
+        size = 500
+        self.dm = randdm(size)
+
+        # bme and gme aren't available in all versions of skbio being benchmarked.
+        # Need to make sure that an ImportError doesn't occur otherwise none of the benchmarks
+        # here will run.
+        self.bme_available = True
+        self.bme = None
+        try:
+            from skbio.tree import bme
+            self.bme = bme
+        except ImportError:
+            self.bme_available = False
+
+        self.gme_available = True
+        self.gme = None
+        try:
+            from skbio.tree import gme
+            self.gme = gme
+        except ImportError:
+            self.gme_available = False
+
+    def time_nj(self):
+        return nj(self.dm)
+
+    @skip_benchmark_if(not self.bme_available)
+    def time_bme(self):
+        return self.bme(self.dm)
+
+    @skip_benchmark_if(not self.gme_available)
+    def time_gme(self):
+        return self.gme(self.dm)
